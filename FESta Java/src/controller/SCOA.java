c2d714e648f30b6bcf340ac10a98ad61b14f5bd1
ivan.alvarenga <ivan.neun@gmail.com>
2019-09-25 11:57:13 -0300

    remoção pasta de script bd e documentação

--- FESta Java/src/controller/SCOA.java
+++ FESta Java/src/controller/SCOA.java
@@ -3,10 +3,14 @@ package controller;
 import view.MainLoginView;
 
 public class SCOA {
-
+	
+	/**
+	 *  Classe principal do projeto, a partir daqui será chamada a tela de Login
+	 **/
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// exibe tela de login
 		MainLoginView mlv = new MainLoginView();
 		
 	}
--- FESta Java/src/view/MainLoginView.java
+++ FESta Java/src/view/MainLoginView.java
@@ -9,11 +9,16 @@ import javax.swing.border.EmptyBorder;
 public class MainLoginView extends JFrame {
 	
 	/**
-	 * 
+	 *  Tela inicial da aplicação, conterá os campos para o usuário logar na aplicação
 	**/
 	private static final long serialVersionUID = 1L;
 
 		public MainLoginView() {
+			this.initUI();
+		}
+		
+		// inicia a parte gráfica da aplicação
+		private void initUI() {
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			setBounds(100, 100, 450, 300);
 			JPanel contentPane = new JPanel();
