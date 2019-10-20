b637d9b7aa1582bd44e645307d9685fc8e22ad4d
ivan.alvarenga <ivan.neun@gmail.com>
2019-10-20 12:54:37 -0300

    adição de componentes de login para HBox

--- FESta Java/src/controller/LoginController.java
+++ FESta Java/src/controller/LoginController.java
@@ -32,75 +32,77 @@ import model.Usuario;
  */
 public class LoginController implements Initializable {
     
-    @FXML private Label label;
+    @FXML private Label erroLoginLabel;
     @FXML private TextField txEmail;
     @FXML private PasswordField txSenha;
     @FXML private Button btSair;
     @FXML private Button btEntrar;
     
+    private Session session;
     
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        btEntrar.setOnMouseClicked((MouseEvent e)->{
-            //System.out.println("Entrei");
+        
+    	btEntrar.setOnMouseClicked((MouseEvent e)->{        	
+            // faz o login
             logar();
         });
         
         btSair.setOnMouseClicked((MouseEvent e)->{
-            //System.out.println("Sai");
+        	// fecha a janela
             fecha();
         });
         
-    }    
+        // pega a session do hibernate
+        session = HibernateUtil.getSession();        
+    } 
+    
     public void fecha(){
         Login.getStage().close();
     }
     
-    public void logar(){
-        
+    public void logar(){	
+    	
     	String username = txEmail.getText();
     	String password = txSenha.getText();
-    	
-    	// pega a sess�o do hibernate
-    	Session session = HibernateUtil.getSession();
-    	// cria caso default de usuario caso n�o exista no banco
+    	    	
+    	// cria caso default de usuario caso nao exista no banco
     	Usuario user = null;
     	
     	if (session != null) {
     		try {
-    			// tenta buscar um usu�rio no banco pelo username
     			
+    			// tenta buscar um usuario no banco pelo username
 				@SuppressWarnings("unchecked")
 				List<Usuario> results = session.createQuery("from Usuario where username = :name").setParameter("name", username).list();
-				
-    			
+				 
+				System.out.println("session is not null");
     			// verifica se o usuario existe
     			if (!results.isEmpty()) {
     				
-    				// pega o usuario com username unico - a unicidade do username deve ser garantida na inser��o
+    				System.out.println("results not empty");
+    				// pega o usuario com username unico - a unicidade do username deve ser garantida na inseridos
     				user = results.get(0);
     			
 	    			if (password.equals(user.getSenha())) {
-	    				 
+	    				
+	    				System.out.println("user e senha certos");
 	    				Principal p = new Principal();
 	    	            fecha();
 	    	            
 	    	            p.start(new Stage());
-	    			} else {
-	    				Alert alert = new Alert(AlertType.ERROR);
-	    	            alert.setHeaderText("Erro");
-	    	            alert.setTitle("Erro");
-	    	            alert.setContentText("Usuario ou Senha Invalido(s)");
-	    	            alert.show();
-	    			}
-	    			
+	    	            
+	    			} else {	    				
+	    				
+	    				this.erroLoginLabel.setVisible(true);
+	    			}	    			
+    			} else {
+    				this.erroLoginLabel.setVisible(true);
     			}
     		} catch (Exception exception) {
     			System.out.println("Exception occred while reading user data: " + exception.getMessage());    			
 		   }
-    	} 
-    	
-    	
+    	}     	    	
     }
 }
