4549dedfbeec6a8b2a1d5575fb7088883c5b2000
Lucas-PC <lucas.gm56@gmail.com>
2019-12-05 21:14:07 -0300

    Add HistoricoView

---
+++ FESta Java/src/model/HistoricoView.java
@@ -0,0 +1,60 @@
+package model;
+
+public class HistoricoView {
+    Integer alunoId, turmaId, frequencia, creditos;
+    Double nota;
+    String nomeAluno, codigoTurma, semestre, ano, resultado;
+
+    public HistoricoView(Integer alunoId, Integer turmaId, Integer frequencia, Integer creditos, Double nota, String nomeAluno, String codigoTurma, String semestre, String ano, String resultado) {
+        this.alunoId = alunoId;
+        this.turmaId = turmaId;
+        this.frequencia = frequencia;
+        this.creditos = creditos;
+        this.nota = nota;
+        this.nomeAluno = nomeAluno;
+        this.codigoTurma = codigoTurma;
+        this.semestre = semestre;
+        this.ano = ano;
+        this.resultado = resultado;
+    }
+
+    public Integer getAlunoId() {
+        return alunoId;
+    }
+
+    public Integer getTurmaId() {
+        return turmaId;
+    }
+
+    public Integer getFrequencia() {
+        return frequencia;
+    }
+
+    public Integer getCreditos() {
+        return creditos;
+    }
+
+    public Double getNota() {
+        return nota;
+    }
+
+    public String getNomeAluno() {
+        return nomeAluno;
+    }
+
+    public String getCodigoTurma() {
+        return codigoTurma;
+    }
+
+    public String getSemestre() {
+        return semestre;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public String getResultado() {
+        return resultado;
+    }
+}
